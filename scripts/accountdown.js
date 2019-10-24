6744d9f7628ea84d663f3fc517d3a943a8cc36ac
André Engen <zornarka@gmail.com>
2019-10-24 20:22:01 +0200

    Fixed a file path

--- scripts/accountdown.js
+++ scripts/accountdown.js
@@ -1,14 +1,14 @@
 let CDDate = new Date("March 20, 2020 00:00:00").getTime();
 
-console.log("CDDate = " + CDDate);
+//console.log("CDDate = " + CDDate);
 
 let x = setInterval(function() {
     let now = new Date().getTime();
-    console.log("now = " + now);
+    //console.log("now = " + now);
 
     //calculate time left
     let distance = CDDate - now;
-    console.log("distance = " + distance);
+    //console.log("distance = " + distance);
 
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
